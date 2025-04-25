07ffe2024fdb4037d91510c09f1afd4506063ff2
AndrewKust <deradus@ukr.net>
2025-04-25 17:23:58 +0300

    UserRegistrationModel

---
+++ TESTTASK/TESTTASK/Models/UserRegistrationModel.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+//MARK: Data model for registering new users
+struct UserRegistrationModel {
+    let name: String
+    
+    let email: String
+    
+    let phone: String
+    
+    let positionId: Int
+}
